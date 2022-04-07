2a1804f9e7c20f7653fc570156883261ddbd5dc6
Deepika Mahadev <deepthideepakm@gmail.com>
2022-04-07 11:00:09 +0200

    my first successful commit

--- src/test/java/com/begins/Flip.java
+++ src/test/java/com/begins/Flip.java
@@ -3,9 +3,11 @@ package com.begins;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,11 +22,12 @@ public class Flip {
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
-		driver.findElement(By.className("_2IX_2- VJZDxU")).sendKeys("deepthideepakm@gmail.com");
-		driver.findElement(By.className("_2IX_2- _3mctLh VJZDxU")).sendKeys("manager");
-		driver.findElement(By.className("_2KpZ6l _2HKlqd _3AWRsL")).click();
-		WebElement error=driver.findElement(By.cssSelector("._2YULOR"));
-		Assert.assertEquals(error,"Your username or password is incorrect");
+		driver.findElement(By.className("_2doB4z")).click();
+        driver.findElement(By.className("_3704LK")).sendKeys("Iphone"+ Keys.ENTER);
+        WebElement women=driver.findElement(By.cssSelector("div[class='_1kidPb']>:nth-child(4)"));
+        Actions builder=new Actions(driver);
+        builder.moveToElement(women).click().build().perform();
+        
 		
 	}
 
